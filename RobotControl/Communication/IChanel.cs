7605bec2d30e8b79233323613f4af60c90771606
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2018-11-07 22:51:59 +0100

    Implemented serial communication chanel

---
+++ RobotControl/Communication/IChanel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RobotControl.Communication
+{
+  public interface IChanel : IDisposable
+  {
+    void Open();
+    void Close();
+    void Send(string data);
+    event EventHandler<IDataReceivedEventArgs> DataReceived;
+  }
+
+  public interface IDataReceivedEventArgs
+  {
+    string Data { get; }
+  }
+
+  public class DataReceivedEventArgs : IDataReceivedEventArgs
+  {
+    string data;
+
+    public DataReceivedEventArgs(string data)
+    {
+      this.data = data;
+    }
+
+    public string Data
+    {
+      get { return data; }
+    }
+  }
+}
---
+++ RobotControl/Communication/Serial.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace RobotControl.Communication
+{
+  public class Serial : IChanel
+  {
+    bool disposed = false;
+
+    private SerialPort port;
+
+    public event EventHandler<IDataReceivedEventArgs> DataReceived;
+
+    public void Close()
+    {
+      if (port != null)
+      {
+        if (port.IsOpen)
+        {
+          port.Close();
+        }
+        
+        port.Dispose();
+        port = null;
+      }
+    }
+
+    public void Open()
+    {
+      Close();
+      try
+      {
+        port = new SerialPort("COM1");
+        port.BaudRate = 76800;
+        port.Open();
+        port.DataReceived += Port_DataReceived;
+      }
+      catch (Exception)
+      {
+        Close();
+        throw;
+      }
+    }
+
+    private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+      SerialPort sp = (SerialPort)sender;
+      var data = sp.ReadExisting();
+      DataReceived?.Invoke(this, new DataReceivedEventArgs(data));
+    }
+
+    public void Send(string data)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (disposed)
+      {
+        return;
+      }
+        
+      if (disposing)
+      {
+        Close();
+      }
+
+      disposed = true;
+    }
+  }
+}
