57e8152375bf14d4dcfd8e9f230193fbd422e636
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2018-11-13 15:29:49 +0100

    Modified simulation model

---
+++ RobotControl/Simulation/IPositionAwareItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotControl.Simulation
+{
+  public interface IPositionAwareItem
+  {
+    Point2D Position { get; set; }
+  }
+}
