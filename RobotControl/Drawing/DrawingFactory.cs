bb39b4cfb8fa04111cd60d2fa1b202f05872b804
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2018-11-15 09:18:09 +0100

    Modified DrawingFactory

--- RobotControl/Drawing/DrawingFactory.cs
+++ RobotControl/Drawing/DrawingFactory.cs
@@ -7,6 +7,7 @@ namespace RobotControl.Drawing
 {
   public static class DrawingFactory
   {
+    private static readonly IDictionary<Type, Func<ISimulationItem, IDrawElement>> cache = new Dictionary<Type, Func<ISimulationItem, IDrawElement>>();
     private static readonly IDictionary<Type, Func<ISimulationItem, IDrawElement>> elements = new Dictionary<Type, Func<ISimulationItem, IDrawElement>>();
 
     static DrawingFactory()
@@ -16,11 +17,18 @@ namespace RobotControl.Drawing
 
     public static IDrawElement GetDrawingInstance(ISimulationItem simulationItem)
     {
+      var type = simulationItem.GetType();
+      if (cache.ContainsKey(type))
+      {
+        return cache[type](simulationItem);
+      }
+
       foreach (var key in elements.Keys)
       {
-        if (simulationItem.GetType().GetInterfaces().Contains(key))
+        if (type.GetInterfaces().Contains(key))
         {
           var action = elements[key];
+          cache.Add(type, action);
           return action(simulationItem);
         }
       }
