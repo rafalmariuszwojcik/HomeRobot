c34946aec3bc8734a0a7b4274451fe0286d6fa63
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2018-11-29 17:18:36 +0100

    Update AreaViewControl.cs

--- RobotControl/Controls/AreaViewControl.cs
+++ RobotControl/Controls/AreaViewControl.cs
@@ -9,14 +9,57 @@ namespace RobotControl.Controls
 {
   public partial class AreaViewControl : Control
   {
+    private ScrollBar hScrollBar;
+    private ScrollBar vScrollBar;
     private int viewZoom = 100;
     private Point2D originPoint = new Point2D(0, 0, MeasurementUnit.Milimeter);
     private Point? previousMousePosition;
     private ISimulation simulation = new Simulation.Simulation();
-    
+
+    private class UpdatingVScrollBar : VScrollBar
+    {
+      protected override void OnValueChanged(EventArgs e)
+      {
+        base.OnValueChanged(e);
+        // setting the scroll position programmatically shall raise Scroll
+        this.OnScroll(new ScrollEventArgs(ScrollEventType.EndScroll, this.Value));
+      }
+    }
+
+    private class UpdatingHScrollBar : HScrollBar
+    {
+      protected override void OnValueChanged(EventArgs e)
+      {
+        base.OnValueChanged(e);
+        // setting the scroll position programmatically shall raise Scroll
+        this.OnScroll(new ScrollEventArgs(ScrollEventType.EndScroll, this.Value));
+      }
+    }
+
     public AreaViewControl()
     {
       InitializeComponent();
+
+
+
+      this.hScrollBar = new UpdatingHScrollBar();
+      this.hScrollBar.Top = this.Height - this.hScrollBar.Height;
+      this.hScrollBar.Left = 0;
+      this.hScrollBar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+      this.vScrollBar = new UpdatingVScrollBar();
+      this.vScrollBar.Top = 0;
+      this.vScrollBar.Left = this.Width - this.vScrollBar.Width;
+      this.vScrollBar.Anchor = AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
+
+      this.hScrollBar.Width = this.Width - this.vScrollBar.Width;
+      this.vScrollBar.Height = this.Height - this.hScrollBar.Height;
+
+      this.Controls.Add(this.hScrollBar);
+      this.Controls.Add(this.vScrollBar);
+
+
+
       //AutoScroll = true;
       //AutoScrollMinSize = CalcAreaSize();
       DoubleBuffered = true;
