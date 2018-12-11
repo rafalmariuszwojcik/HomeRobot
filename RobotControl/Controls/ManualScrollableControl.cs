bbd0e5f9fac2b682aac1639620ccf2e8f351a239
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2018-12-11 19:32:31 +0100

    Manual scrolable control implemented

---
+++ RobotControl/Controls/ManualScrollableControl.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Forms;
+
+namespace RobotControl.Controls
+{
+  public class ManualScrollableControl : ScrollableControl
+  {
+    private const int WM_HSCROLL = 0x114;
+    private const int WM_VSCROLL = 0x115;
+  }
+}
