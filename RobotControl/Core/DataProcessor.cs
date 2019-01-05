ac4a8b1e130ba20be8f1ebeb90279275c9828928
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2019-01-05 16:47:53 +0100

    Added new data processor class

---
+++ RobotControl/Core/DataProcessor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RobotControl.Core
+{
+  public class DataProcessor<T> : DisposableBase
+    where T: class
+  {
+    private readonly object lockData = new object();
+    private readonly CancellationTokenSource tokenSource;
+    private readonly CancellationToken token;
+    private readonly Task worker;
+    private readonly ManualResetEvent signal;
+    private readonly Queue<T> data = new Queue<T>();
+    private readonly Action<T> action;
+
+    public DataProcessor(Action<T> action)
+    {
+      tokenSource = new CancellationTokenSource();
+      token = tokenSource.Token;
+      signal = new ManualResetEvent(true);
+      this.action = action;
+      worker = Task.Run(() => ProcessData(), token);
+    }
+
+    public void Enqueue(T item)
+    {
+      lock (lockData)
+      {
+        data.Enqueue(item);
+        signal.Set();
+      }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      tokenSource?.Cancel();
+      signal?.Set();
+      worker?.Wait();
+      worker?.Dispose();
+      signal?.Dispose();
+      tokenSource?.Dispose();
+    }
+
+    private void ProcessData()
+    {
+      T item = null;
+      token.ThrowIfCancellationRequested();
+      while (true)
+      {
+        signal.WaitOne();
+        do
+        {
+          lock (lockData)
+          {
+            item = data.Any() ? data.Dequeue() : null;
+          }
+
+          if (item != null)
+          {
+            action(item);
+          }
+
+          if (token.IsCancellationRequested)
+          {
+            token.ThrowIfCancellationRequested();
+          }
+        }
+        while (item != null);
+
+        lock (lockData)
+        {
+          if (!data.Any())
+          {
+            signal.Reset();
+          }
+        }
+      }
+    }
+  }
+}
---
+++ RobotControl/Core/DisposableBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RobotControl.Core
+{
+  public abstract class DisposableBase : IDisposable
+  {
+    private bool disposed = false;
+
+    public DisposableBase()
+    {
+    }
+
+    ~DisposableBase()
+    {
+      InternalDispose(false);
+    }
+
+    public void Dispose()
+    {
+      InternalDispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    protected abstract void Dispose(bool disposing);
+
+    private void InternalDispose(bool disposing)
+    {
+      if (disposed)
+      {
+        return;
+      }
+
+      if (disposing)
+      {
+        Dispose(disposing);
+      }
+
+      disposed = true;
+    }
+  }
+}
