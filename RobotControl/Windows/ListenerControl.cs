92a2e5ddad44c94cdfa01c9fd481e442103c51ae
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2019-01-08 16:48:18 +0100

    Listener control added, this class is designed to process messages and commands from communication chanels

---
+++ RobotControl/Windows/ListenerControl.cs
@@ -0,0 +1,49 @@
+﻿using RobotControl.Command;
+using RobotControl.Communication;
+using RobotControl.Core;
+using System.Windows.Forms;
+
+namespace RobotControl.Windows
+{
+  public class ListenerControl : DisposableBase, IListener<string>, IListener<ICommand>
+  {
+    private readonly Control control;
+
+    public ListenerControl(Control control)
+    {
+      this.control = control;
+    }
+
+    public void MessageReceived(IChannel channel, string data)
+    {
+      ProcessMessage(channel, data);
+    }
+
+    public void MessageReceived(IChannel channel, ICommand data)
+    {
+      ProcessMessage(channel, data);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      ;
+    }
+
+    private void ProcessMessage<T>(IChannel channel, T data) where T: class
+    {
+      if (data == null)
+      {
+        return;
+      }
+
+      if (control.InvokeRequired)
+      {
+        //control.BeginInvoke(new Action<object, ICommand>(CommandReceived), new[] { sender, command });
+      }
+      else
+      {
+        //CommandReceived(sender, command);
+      }
+    }
+  }
+}
