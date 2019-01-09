732ad386815250a7c76c510eab06be3f1b951e03
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2019-01-09 16:46:26 +0100

    Update ListenerControl.cs

--- RobotControl/Windows/ListenerControl.cs
+++ RobotControl/Windows/ListenerControl.cs
@@ -1,6 +1,7 @@
 ﻿using RobotControl.Command;
 using RobotControl.Communication;
 using RobotControl.Core;
+using System;
 using System.Windows.Forms;
 
 namespace RobotControl.Windows
@@ -16,20 +17,23 @@ namespace RobotControl.Windows
 
     public void MessageReceived(IChannel channel, string data)
     {
-      ProcessMessage(channel, data);
+      ProcessMessage(channel, data, (c, d) => InvokeOnMessageReceived(c, d));
     }
 
     public void MessageReceived(IChannel channel, ICommand data)
     {
-      ProcessMessage(channel, data);
+      ProcessMessage(channel, data, (c, d) => InvokeOnCommandReceived(c, d));
     }
+
+    public event EventHandler<MessageEventArgs> OnMessageReceived;
+    public event EventHandler<CommandEventArgs> OnCommandReceived;
 
     protected override void Dispose(bool disposing)
     {
       ;
     }
 
-    private void ProcessMessage<T>(IChannel channel, T data) where T: class
+    private void ProcessMessage<T>(IChannel channel, T data, Action<IChannel, T> action) where T: class
     {
       if (data == null)
       {
@@ -38,12 +42,52 @@ namespace RobotControl.Windows
 
       if (control.InvokeRequired)
       {
-        //control.BeginInvoke(new Action<object, ICommand>(CommandReceived), new[] { sender, command });
+        control.BeginInvoke(action, new[] { (object)channel, data });
       }
       else
       {
-        //CommandReceived(sender, command);
+        action(channel, data);
       }
     }
+
+    private void InvokeOnMessageReceived(IChannel channel, string data)
+    {
+      OnMessageReceived?.Invoke(this, new MessageEventArgs(channel, data));
+    }
+
+    private void InvokeOnCommandReceived(IChannel channel, ICommand command)
+    {
+      OnCommandReceived?.Invoke(this, new CommandEventArgs(channel, command));
+    }
+  }
+
+  public abstract class MessageEventBaseArgs : EventArgs
+  {
+    public MessageEventBaseArgs(IChannel channel)
+    {
+      Channel = channel;
+    }
+
+    public IChannel Channel { get; }
+  }
+
+  public class MessageEventArgs : MessageEventBaseArgs
+  {
+    public MessageEventArgs(IChannel channel, string message) : base(channel)
+    {
+      Message = message;
+    }
+
+    public string Message { get; }
+  }
+
+  public class CommandEventArgs : MessageEventBaseArgs
+  {
+    public CommandEventArgs(IChannel channel, ICommand command) : base(channel)
+    {
+      Command = command;
+    }
+
+    public ICommand Command { get; }
   }
 }
