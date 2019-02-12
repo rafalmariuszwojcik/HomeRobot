227705793a6058013e96a460cb48eb15cc9c13e6
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2019-02-12 18:14:28 +0100

    Refactoring of ManagerBase

--- RobotControl/Core/ManagerBase.cs
+++ RobotControl/Core/ManagerBase.cs
@@ -1,9 +1,8 @@
 ﻿using RobotControl.Communication;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using System.Threading.Tasks;
 
 namespace RobotControl.Core
 {
@@ -15,11 +14,10 @@ namespace RobotControl.Core
     private readonly object lockObject = new object();
     private readonly IList<T> listeners = new List<T>();
     private readonly DataProcessingQueue<M> messageQueue;
-    private readonly IDictionary<Type, ListenerInfo> types = new Dictionary<Type, ListenerInfo>();
-
+    
     protected ManagerBase(int interval = 0)
     {
-      messageQueue = new DataProcessingQueue<M>(x => PostMessage(null, x), interval);
+      messageQueue = new DataProcessingQueue<M>(x => PostData(null, x), interval);
     }
 
     protected IList<IDisposable> Disposables { get; } = new List<IDisposable>();
@@ -67,7 +65,18 @@ namespace RobotControl.Core
       action?.Invoke();
     }
 
-    private void PostMessage(IChannel channel, IEnumerable<M> data)
+    protected virtual IEnumerable<Action> SendDataActions(T listener, IEnumerable<M> data)
+    {
+      var result = new List<Action>();
+      if (listener is IListener<M> listenerInterface)
+      {
+        result.Add(new Action(() => listenerInterface.DataReceived(null, data)));
+      }
+
+      return result;
+    }
+
+    private void PostData(IChannel channel, IEnumerable<M> data)
     {
       List<T> listenersToProcess;
       lock (lockObject)
@@ -75,10 +84,7 @@ namespace RobotControl.Core
         listenersToProcess = new List<T>(listeners);
       }
 
-      foreach (var listener in listenersToProcess)
-      {
-        ProcessListener(listener, data);
-      }
+      Parallel.ForEach(listenersToProcess, x => ProcessListener(x, data));
     }
 
     private void ProcessListener(T listener, IEnumerable<M> data)
@@ -88,66 +94,8 @@ namespace RobotControl.Core
         return;
       }
 
-      /* Test
-      if (data.Any() && data.First() is ICommand)
-      {
-        var x = 0;
-        var y = x;
-      }
-      */
-
-      var listenerInfo = GetListenerInfo(listener);
-      foreach (var intf in listenerInfo.Interfaces)
-      {
-        var dataToSend = (IList)Activator.CreateInstance(intf.PackageType);
-        foreach (var item in data.Where(x => x.GetType().Equals(intf.DataType) || x.GetType().GetInterfaces().Contains(intf.DataType)))
-        {
-          dataToSend.Add(item);
-        }
-
-        var action = new Action(() => intf.Method.Invoke(listener, new object[] { null, dataToSend }));
-        SendData(listener, action);
-      }
-    }
-
-    private ListenerInfo GetListenerInfo(T listener)
-    {
-      var listenerType = listener.GetType();
-      if (!types.ContainsKey(listenerType))
-      {
-        var listenerInfo = new ListenerInfo { Interfaces = new List<InterfaceInfo>() };
-        var interfaces = listenerType.GetInterfaces().Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IListener<>));
-        foreach (var intf in interfaces)
-        {
-          var dataType = intf.GetGenericArguments()?.Count() == 1 ? intf.GetGenericArguments()[0] : null;
-          if (dataType != null)
-          {
-            var packageType = typeof(List<>).MakeGenericType(dataType);
-            var method = intf.GetMethod("DataReceived");
-            if (packageType != null && method != null)
-            {
-              listenerInfo.Interfaces.Add(new InterfaceInfo { DataType = dataType, PackageType = packageType, Method = method });
-            }
-          }
-        }
-
-        types.Add(listenerType, listenerInfo);
-        return listenerInfo;
-      }
-
-      return types[listenerType];
-    }
-
-    private class ListenerInfo
-    {
-      internal IList<InterfaceInfo> Interfaces { get; set; }
-    }
-
-    private class InterfaceInfo
-    {
-      internal Type DataType { get; set; }
-      internal Type PackageType { get; set; }
-      internal MethodInfo Method { get; set; }
+      var actions = SendDataActions(listener, data);
+      Parallel.ForEach(actions, x => SendData(listener, x));      
     }
   }
 }
--- RobotControl/Windows/ControlManager.cs
+++ RobotControl/Windows/ControlManager.cs
@@ -2,14 +2,20 @@
 using RobotControl.Core;
 using RobotControl.Messages;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace RobotControl.Windows
 {
   public class ControlManager : ManagerBase<ControlManager, Control, DataPackage>
   {
+    private readonly object listenerInfoLockObject = new object();
+    private readonly IDictionary<Type, ListenerInfo> types = new Dictionary<Type, ListenerInfo>();
+
     public ControlManager()
       : base(20)
     {
@@ -38,6 +44,68 @@ namespace RobotControl.Windows
       {
         base.SendData(listener, action);
       }
+    }
+
+    protected override IEnumerable<Action> SendDataActions(Control listener, IEnumerable<DataPackage> data)
+    {
+      var result = new List<Action>();
+      var listenerInfo = GetListenerInfo(listener);
+      foreach (var intf in listenerInfo.Interfaces)
+      {
+        var dataToSend = (IList)Activator.CreateInstance(intf.PackageType);
+        foreach (var item in data.Where(x => x.GetType().Equals(intf.DataType) || x.GetType().GetInterfaces().Contains(intf.DataType)))
+        {
+          dataToSend.Add(item);
+        }
+
+        var action = new Action(() => intf.Method.Invoke(listener, new object[] { null, dataToSend }));
+        result.Add(action);
+      }
+
+      return result;
+    }
+
+    private ListenerInfo GetListenerInfo(Control listener)
+    {
+      lock (listenerInfoLockObject)
+      {
+        var listenerType = listener.GetType();
+        if (!types.ContainsKey(listenerType))
+        {
+          var listenerInfo = new ListenerInfo { Interfaces = new List<InterfaceInfo>() };
+          var interfaces = listenerType.GetInterfaces().Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IListener<>));
+          foreach (var intf in interfaces)
+          {
+            var dataType = intf.GetGenericArguments()?.Count() == 1 ? intf.GetGenericArguments()[0] : null;
+            if (dataType != null)
+            {
+              var packageType = typeof(List<>).MakeGenericType(dataType);
+              var method = intf.GetMethod(nameof(IListener<object>.DataReceived));
+              if (packageType != null && method != null)
+              {
+                listenerInfo.Interfaces.Add(new InterfaceInfo { DataType = dataType, PackageType = packageType, Method = method });
+              }
+            }
+          }
+
+          types.Add(listenerType, listenerInfo);
+          return listenerInfo;
+        }
+
+        return types[listenerType];
+      }
+    }
+
+    private class ListenerInfo
+    {
+      internal IList<InterfaceInfo> Interfaces { get; set; }
+    }
+
+    private class InterfaceInfo
+    {
+      internal Type DataType { get; set; }
+      internal Type PackageType { get; set; }
+      internal MethodInfo Method { get; set; }
     }
   }
 }
