01ac8bd306c0e12783a5047d2b1481177da6da95
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2019-02-18 19:11:24 +0100

    Implement position calculator

---
+++ RobotControl/Simulation/Robot/PositionCalculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RobotControl.Simulation.Robot
+{
+  public enum Encoder
+  {
+    Left = 0,
+    Right = 1,
+  }
+
+  public class PositionCalculator
+  {
+    private readonly double robotWidth;
+    private readonly double wheelRadius;
+    private readonly int encoderPoints;
+    private readonly double oneHoleDistance;
+    private readonly SignalData[] encoders = new SignalData[(int)Encoder.Right + 1] { null, null };
+    
+    public PositionCalculator(double robotWidth, double wheelRadius, int encoderPoints)
+    {
+      this.robotWidth = robotWidth;
+      this.wheelRadius = wheelRadius;
+      this.encoderPoints = encoderPoints;
+      oneHoleDistance = (wheelRadius * 2.0 * Math.PI) / encoderPoints;
+    }
+
+    public void Signal(Encoder encoder, int count, long milis)
+    {
+      if (encoders[(int)encoder] != null)
+      {
+        var timeDelta = (milis - encoders[(int)encoder].Milis) / 1000.0;
+        var distance = (count - encoders[(int)encoder].Count) * oneHoleDistance;
+        var speed = distance / timeDelta;
+
+      }
+
+      encoders[(int)encoder] = new SignalData(count, milis);
+    }
+
+    private class SignalData
+    {
+      internal SignalData(int count, long milis)
+      {
+        Count = count;
+        Milis = milis;
+      }
+
+      internal int Count { get; private set; }
+      internal long Milis { get; private set; }
+    }
+  }
+}
--- RobotControlTest/RobotTest.cs
+++ RobotControlTest/RobotTest.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using RobotControl.Simulation.Robot;
+using System;
+using System.Threading.Tasks;
 
 namespace Tests
 {
@@ -13,7 +15,22 @@ namespace Tests
     [Test]
     public void Test1()
     {
+      const double ROBOT_WIDTH = 124.0;
+      const double WHEEL_RADIUS = 33.2;
+      const int ENCODER_POINTS = 20;
+
+      var calculator = new PositionCalculator(ROBOT_WIDTH, WHEEL_RADIUS, ENCODER_POINTS);
+
       var robot = new Robot();
+      var start = DateTime.Now;
+      for (var i = 1; i <= 10; i++)
+      {
+        calculator.Signal(Encoder.Left, i, (long)(DateTime.Now - start).TotalMilliseconds);
+        Task.Delay(10).Wait();
+        calculator.Signal(Encoder.Right, i, (long)(DateTime.Now - start).TotalMilliseconds);
+        Task.Delay(90).Wait();
+      }
+      
       Assert.Pass();
     }
   }
