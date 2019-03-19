627bc996a3c332fd955fd853535ada45b9d18cff
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2019-03-19 20:08:04 +0100

    Update RobotControl.cs

--- RobotControl/RobotControl.cs
+++ RobotControl/RobotControl.cs
@@ -102,18 +102,18 @@ namespace RobotControl
       Task.Factory.StartNew(new Action(() => 
       {
         var list = new List<long>();
-        for (var i = 0; i < 200; i++)
+        for (var i = 0; i < 300; i++)
         {
           var totalMilliseconds = (long)new TimeSpan(DateTime.Now.Ticks).TotalMilliseconds;
           MessageManager.Instance.DataReceived(this, new[] { $"ENC,0,{i},{totalMilliseconds},2;{Environment.NewLine}" });
 
-          Thread.Sleep(10);
+          Thread.Sleep(15);
 
           MessageManager.Instance.DataReceived(this, new[] { $"ENC,1,{i},{totalMilliseconds},2;{Environment.NewLine}" });
           list.Add(totalMilliseconds);
 
           // Dont use Task.Delay, it is very not precise.
-          Thread.Sleep(30);
+          Thread.Sleep(25);
         }
 
         for (var i = 0; i < list.Count() - 1; i++)
