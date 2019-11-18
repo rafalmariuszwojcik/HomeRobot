59c5212d4694849ded25e497712274d2ea54d9dc
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2019-11-18 20:30:32 +0100

    Added IRobotCommand

---
+++ RobotControl/Command/Robot/IRobotCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotControl.Command.Robot
+{
+  /// <summary>
+  /// Robot command interface.
+  /// </summary>
+  /// <seealso cref="RobotControl.Command.ICommand" />
+  public interface IRobotCommand : ICommand
+  {
+  }
+}
