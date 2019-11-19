f1f2d73d4739578fdacfe12c832e2fc8407eac51
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2019-11-19 19:55:12 +0100

    Update FakeConfiguration.cs

--- RobotControl/Communication/Fake/FakeConfiguration.cs
+++ RobotControl/Communication/Fake/FakeConfiguration.cs
@@ -1,5 +1,9 @@
 ﻿namespace RobotControl.Communication.Fake
 {
+  /// <summary>
+  /// Fake robot channel configuration.
+  /// </summary>
+  /// <seealso cref="RobotControl.Communication.ConfigurationBase" />
   public class FakeConfiguration : ConfigurationBase
   {
   }
