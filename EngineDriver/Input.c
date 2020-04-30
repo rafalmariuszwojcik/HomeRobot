1d43b09f897449e7cc47db828761bf5735ea9e6e
Rafał Wójcik <44831348+rafalmariuszwojcik@users.noreply.github.com>
2020-04-30 23:07:59 +0200

    Implemented input module

--- EngineDriver/Input.c
+++ EngineDriver/Input.c
@@ -1,8 +1,47 @@
+#include <string.h>
+#include <stdbool.h>
 #include "Input.h"
 
 struct InputBuffer _buffer;
+bool _Input_IsDataValid(char data);
+
+void Input_Initialize(uint8_t (*available_ptr)(), char (*read_ptr)())
+{
+  memset(&_buffer, 0, sizeof(struct InputBuffer));
+  _buffer.available_ptr = available_ptr;
+  _buffer.read_ptr = read_ptr;
+  _buffer.dataIndex = -1;
+}
 
 char* Input_GetData() 
 {
-  return NULL;	
+  while (_buffer.available_ptr != NULL && _buffer.available_ptr() > 0)
+  {
+    char data = _buffer.read_ptr != NULL ? _buffer.read_ptr() : 0;
+    if (_Input_IsDataValid(data) && _buffer.dataIndex <= _INPUT_BUFFER_SIZE - 3)
+    {
+      _buffer.data[++_buffer.dataIndex] = data;
+      _buffer.data[_buffer.dataIndex + 1] = 0;
+    }
+    else 
+    {
+      _buffer.dataIndex = -1;
+      break;
+    }
+  }
+
+  if (_buffer.dataIndex >= 0 && _buffer.data[_buffer.dataIndex] == ';')
+  {
+    _buffer.dataIndex = -1;
+    return &(_buffer.data[0]);
+  }
+  else 
+  {
+    return NULL;
+  }
+}
+
+bool _Input_IsDataValid(char data)
+{
+  return (data == ',') || (data == ';') || (data == 32) || (data >= 48 && data <= 57) || (data >= 65 && data <= 90) || (data >= 97 && data <= 122);
 }
--- EngineDriver/Input.h
+++ EngineDriver/Input.h
@@ -1,16 +1,18 @@
 #include <inttypes.h>
-#include <stdbool.h>
 
 #ifndef INPUT_H_
 #define INPUT_H_
 
-const uint8_t InputBufferSize = 64;
+enum { _INPUT_BUFFER_SIZE = 64 };
 
 struct InputBuffer {
+  uint8_t (*available_ptr)();
+  char (*read_ptr)();
   int8_t dataIndex;
-  char[InputBufferSize] data;
+  char data[_INPUT_BUFFER_SIZE];
 };
 
+void Input_Initialize(uint8_t (*available_ptr)(), char (*read_ptr)());
 char* Input_GetData();
 
 #endif
